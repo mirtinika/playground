d26940dac4dc983a2c7de46b5f526b9acb1f3dec
Mireya <mireiacr@gmail.com>
2018-10-22 12:34:10 +0200

    Adding Routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Route, BrowserRouter, Link } from 'react-router-dom'
 import Cube from './components/Cube'
 import Parallax from './components/Parallax'
 import './App.css'
@@ -6,14 +7,21 @@ import './App.css'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Cube />
-        </div>
-        <div>
-          <Parallax />
-        </div>
-      </div>
+      <BrowserRouter>
+        <main>
+          <div>
+            <Link to="/parallax">Parallax</Link> ||
+            <Link to="/cube">Cube</Link>
+          </div>
+          <div className="App">
+            <div className="container">
+              <Route path="/" exact={true} component={Cube} />
+              <Route path="/cube" exact={true} component={Cube} />
+            </div>
+            <Route path="/parallax" exact={true} component={Parallax} />
+          </div>
+        </main>
+      </BrowserRouter>
     )
   }
 }
